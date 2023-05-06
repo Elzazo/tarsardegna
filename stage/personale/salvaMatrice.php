de52a915705784dba39facdff30ac23f421754bb
Elzazo <aldolezza@gmail.com>
2023-05-06 16:07:17 +0200

    saves all but "scadenze", code for revisions added

--- stage/personale/salvaMatrice.php
+++ stage/personale/salvaMatrice.php
@@ -3,11 +3,23 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
   $mese = $_GET['mese'];
   $json = $_GET['matrice'];
   $matrix = json_decode($json, true);
-  $result = file_put_contents("data/".$mese."_saved.json", $json);
+  
+  $i = 1;
+  $old_version="";
+  while (true) {
+	  $old_version="data/".$mese."_rev".str_pad($i, 2, "0", STR_PAD_LEFT).".json";
+	  if (file_exists($old_version)) {
+		  $i++;
+	  }else {
+		  break;
+	  }
+  }
+  rename("data/".$mese.".json", $old_version);
+  $result = file_put_contents("data/".$mese.".json", $json);
   if ($result === false) {
 	echo "Errore durante il salvataggio del file.";
   } else {
-	echo "Salvataggio completato con successo.";
+	echo "Salvataggio completato con successo, versione attuale del file: ".($i+1);
   }
 }
 ?>
