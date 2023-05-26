a999fdb4ed77bf271539564f416c6b52cbb5cdb6
Elzazo <aldolezza@gmail.com>
2023-05-26 07:59:58 +0200

    Login temporary removed

--- www/index.php
+++ www/index.php
@@ -11,7 +11,8 @@
         <section class="clean-block clean-hero" style="color: rgba(9, 162, 255, 0.85);background: url(&quot;assets/img/Athena.png&quot;);height: 727px;">
             <div class="text">
                 <h2>Benvenuto</h2>
-                <p>Iscriviti con il tuo indirizzo mail o accedi usando mail e password</p><a class="btn btn-outline-light btn-lg" role="button" data-bs-toggle="tooltip" data-bss-tooltip="" href="login.php" title="Clicca per entrare">Entra</a>
+                <!--p>Iscriviti con il tuo indirizzo mail o accedi usando mail e password</p><a class="btn btn-outline-light btn-lg" role="button" data-bs-toggle="tooltip" data-bss-tooltip="" href="login.php" title="Clicca per entrare">Entra</a-->
+                <p>Vai alla sezione Aree per scoprire le webapp disponibili</p><a class="btn btn-outline-light btn-lg" role="button" data-bs-toggle="tooltip" data-bss-tooltip="" href="aree.php" title="Vai alle Aree">Aree</a>
             </div>
         </section>
     </main>
